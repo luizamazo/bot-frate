92b727f2c1f59b5b46a25d74197eb0528c7ec969
luizamazo <luizamazo@hotmail.com>
2021-01-23 02:41:11 -0400

    vpn test

---
+++ vpnTest.js
@@ -0,0 +1,16 @@
+const puppeteer = require('puppeteer-extra')
+
+const StealthPlugin = require('puppeteer-extra-plugin-stealth')
+puppeteer.use(StealthPlugin())
+
+const AdblockerPlugin = require('puppeteer-extra-plugin-adblocker')
+puppeteer.use(AdblockerPlugin({ blockTrackers: true }))
+
+puppeteer.launch({ headless: true }).then(async browser => {
+  const page = await browser.newPage()
+  await page.goto('https://ipleak.net/')
+  await page.waitForTimeout(1000)
+  await page.screenshot({ path: 'vpnTest.png', fullPage: true })
+
+  await browser.close()
+})
